482cf32f860a38fe4261e8fa026b9ce2a156771f
rishiraj007 <rishiraj@mounttalent.com>
2023-06-19 22:57:34 +0530

    [Prabhu]Add: Delete Method to delete corresponding element from the array

---
+++ GenericPracticeProblem/DeleteArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPracticeProblem
+{
+    public class DeleteArray
+    {
+        public static void DeleteMethod<T>(ref T[] array, T elementToDelete)
+        {
+            int index = Array.IndexOf(array, elementToDelete);
+            if (index >= 0)
+            {
+                T[] newArray = new T[array.Length - 1];
+                Array.Copy(array, 0, newArray, 0, index);
+                Array.Copy(array, index + 1, newArray, index, array.Length - index - 1);
+                array = newArray;
+            }
+        }
+    }
+}
--- GenericPracticeProblem/Program.cs
+++ GenericPracticeProblem/Program.cs
@@ -1,10 +1,35 @@
 ﻿namespace GenericPracticeProblem
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Generics Practice Problem !");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nSelect the operation to perform \n1.DeleteMethod \n2.Exit");
+                int options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        int[] intArray = { 1, 2, 3, 4, 5 };
+                        DeleteArray.DeleteMethod(ref intArray, 3);
+                        Console.WriteLine(string.Join(", ", intArray));
+
+                        double[] doubleArray = { 1.5, 2.5, 3.5, 4.5, 5.5 };
+                        DeleteArray.DeleteMethod(ref doubleArray, 4.5);
+                        Console.WriteLine(string.Join(", ", doubleArray));
+
+                        char[] charArray = { 'a', 'b', 'c', 'd', 'e' };
+                        DeleteArray.DeleteMethod(ref charArray, 'b');
+                        Console.WriteLine(string.Join(", ", charArray));
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
